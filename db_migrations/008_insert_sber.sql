cc9a86e9cae8d15f3a26ca40e5713f4717c14edf
Nikolay <nefedovna@student.bmstu.ru>
2025-12-11 15:31:34 +0300

    added insert

---
+++ db_migrations/008_insert_sber.sql
@@ -0,0 +1,5 @@
+insert into ticker(name) values ('SBER');
+
+INSERT INTO public.bsm_params
+(strike, rate, dividend_yield, volatility, maturity_years, ticker_id)
+VALUES(1.0, 1.0, 1.0, 1.0, 1.0, 1);
